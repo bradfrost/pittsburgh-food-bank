f195a932e6036ce0281677c5c947a4ffccab0670
Brad Frost <frostbd@gmail.com>
2014-03-05 13:35:56 -0500

    Update Pattern Lab to latest version

--- core/builder.php
+++ core/builder.php
@@ -1,7 +1,7 @@
 <?php
 
 /*!
- * Pattern Lab Builder CLI - v0.7.2
+ * Pattern Lab Builder CLI - v0.7.8
  *
  * Copyright (c) 2013-2014 Dave Olsen, http://dmolsen.com
  * Licensed under the MIT license
@@ -26,6 +26,12 @@
  * 		Prints out the current version of Pattern Lab.
  *
  */
+
+// check to see if json_decode exists. might be disabled in installs of PHP 5.5
+if (!function_exists("json_decode")) {
+	print "Please check that your version of PHP includes the JSON extension. It's required for Pattern Lab to run. Aborting.\n";
+	exit;
+}
 
 // auto-load classes
 require(__DIR__."/lib/SplClassLoader.php");
